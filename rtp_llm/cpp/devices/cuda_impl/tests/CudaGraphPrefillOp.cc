eaac2754c433619d469351e1a4987896f49da5f7
JackTan25 <tanboyu.tby@alibaba-inc.com>
2025-12-14 17:35:33 +0800

    feat: refactor cuda graph prefill ut

--- rtp_llm/cpp/devices/cuda_impl/tests/CudaGraphPrefillOp.cc
+++ rtp_llm/cpp/devices/cuda_impl/tests/CudaGraphPrefillOp.cc
@@ -2,13 +2,23 @@
 namespace cuda_graph {
 using namespace rtp_llm;
 
-void CudaGraphPrefillOp::init(py::object py_instance) {
-    cuda_graph_runner_ = createCudaGraphRunner(std::move(py_instance));
-    // initializeResource();
+void CudaGraphPrefillOp::init(py::object       py_instance,
+                              int64_t          max_context_batch_size,
+                              int64_t          hidden_size,
+                              int64_t          max_seq_len,
+                              int64_t          tokens_per_block,
+                              int64_t          max_prefill_cuda_graph_len,
+                              std::vector<int> prefill_capture_seq_lens) {
+    cuda_graph_runner_ = createCudaGraphRunner(std::move(py_instance),
+                                               max_context_batch_size,
+                                               hidden_size,
+                                               max_seq_len,
+                                               tokens_per_block,
+                                               prefill_capture_seq_lens);
     // model warm up
-    auto inputs = buildInputs(2, 64, 64, 64, true);
+    auto inputs = buildInputs(2, max_seq_len, max_seq_len, tokens_per_block, true);
     cuda_graph_runner_->normalForward(inputs);
-    cuda_graph_runner_->setMaxPrefillCudaGraphLen(960);
+    cuda_graph_runner_->setMaxPrefillCudaGraphLen(max_prefill_cuda_graph_len);
     cuda_graph_runner_->initCapture();
 }
 
@@ -16,26 +26,21 @@ int CudaGraphPrefillOp::getCurrentRealGraphSize() {
     return cuda_graph_runner_->getCurrentRealGraphBs();
 }
 
-CudaGraphRunnerPtr CudaGraphPrefillOp::createCudaGraphRunner(py::object py_instance) {
+CudaGraphRunnerPtr CudaGraphPrefillOp::createCudaGraphRunner(py::object       py_instance,
+                                                             int64_t          max_context_batch_size,
+                                                             int64_t          hidden_size,
+                                                             int64_t          max_seq_len,
+                                                             int64_t          tokens_per_block,
+                                                             std::vector<int> prefill_capture_seq_lens) {
     DeviceInitParams params;
     DeviceBase*      device                              = rtp_llm::DeviceFactory::getDefaultDevice();
     params.hw_kernel_config.enable_cuda_graph            = true;
-    params.fifo_scheduler_config.max_context_batch_size  = 128;
-    params.hw_kernel_config.enable_cuda_graph_debug_mode = false;
-    params.hidden_size                                   = 3584;
-    params.max_seq_len                                   = 64;
-    params.tokens_per_block                              = 64;
+    params.fifo_scheduler_config.max_context_batch_size  = max_context_batch_size;
     params.hw_kernel_config.enable_cuda_graph_debug_mode = true;
-    params.hw_kernel_config.prefill_capture_seq_lens     = {
-        6,   10,  14,  15,  20,  25,  30,  35,  40,  45,  50,  55,  60,  65,  70,  75,  77,  80,  85,  90,  95,
-        100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200,
-        205, 210, 215, 220, 225, 230, 235, 240, 245, 248, 250, 252, 255, 256, 260, 265, 270, 275, 280, 285, 290,
-        295, 300, 305, 310, 311, 315, 317, 320, 321, 325, 330, 335, 340, 345, 350, 355, 356, 360, 365, 370, 375,
-        380, 385, 390, 395, 399, 400, 405, 410, 411, 415, 420, 425, 430, 435, 440, 445, 450, 455, 460, 465, 470,
-        475, 480, 485, 490, 495, 500, 512, 520, 540, 560, 576, 580, 600, 620, 629, 640, 660, 673, 680, 685, 697,
-        700, 703, 720, 740, 760, 780, 793, 797, 800, 820, 837, 840, 844, 856, 860, 880, 889, 900, 920, 940, 960};
-    // int  layer_num                              = 24;
-    // int  block_num                              = 26037;
+    params.hidden_size                                   = hidden_size;
+    params.max_seq_len                                   = max_seq_len;
+    params.tokens_per_block                              = tokens_per_block;
+    params.hw_kernel_config.prefill_capture_seq_lens     = prefill_capture_seq_lens;
     auto               runner_ptr            = device->getDeviceGraphRunner(params, std::move(py_instance), 0, true);
     CudaGraphRunnerPtr cuda_graph_runner_ptr = dynamic_cast<CudaGraphRunner*>(runner_ptr);
     cuda_graph_runner_ptr->setModelDataType(torch::scalarTypeToTypeMeta(torch::kBFloat16));
@@ -49,9 +54,6 @@ PyModelInputs CudaGraphPrefillOp::buildInputs(int64_t batch_size,
                                               bool    use_max_padded_mode) {
     PyModelInputs inputs;
     inputs.attention_inputs.is_prefill = true;
-    // int  hidden_size                   = 896;
-    // int  layer_num                     = 24;
-    // int  block_num                     = 26037;
 
     // 创建新的序列长度数组：10, 20, 30, ..., 10 * batch_size
     std::vector<int32_t> input_lengths_data(batch_size);
@@ -166,7 +168,15 @@ PyModelInputs CudaGraphPrefillOp::buildInputs(int64_t batch_size,
 PYBIND11_MODULE(libtest_cuda_graph_prefill_ops, m) {
     py::class_<cuda_graph::CudaGraphPrefillOp>(m, "CudaGraphPrefillOp")
         .def(py::init<>())
-        .def("init", &CudaGraphPrefillOp::init)
+        .def("init",
+             &CudaGraphPrefillOp::init,
+             py::arg("py_instance"),
+             py::arg("max_context_batch_size"),
+             py::arg("hidden_size"),
+             py::arg("max_seq_len"),
+             py::arg("tokens_per_block"),
+             py::arg("max_prefill_cuda_graph_len"),
+             py::arg("prefill_capture_seq_lens"))
         .def("forward", &cuda_graph::CudaGraphPrefillOp::forward)
         .def("getCurrentRealGraphSize", &cuda_graph::CudaGraphPrefillOp::getCurrentRealGraphSize)
         .def("buildInputs", &cuda_graph::CudaGraphPrefillOp::buildInputs);
